5746f2a5a8403fc89195b01b91fed44769217258
Derek <legato_5@yahoo.com>
2019-09-18 20:04:51 -0700

    changed from football to Quidditch scoreboard.

--- src/App.js
+++ src/App.js
@@ -8,37 +8,51 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [hScore, setHscore] = useState(0);
   const [aScore, setAscore] = useState(0);
-
+  let min = 0;
+  let max = 10;
   //functions for changing scores
-  const hTouchdown = () => {
-    setHscore(hScore + 7);
+  
+  const hGoal = () => {
+    setHscore(hScore + 10);
   }
 
-  const aTouchdown = () => {
-    setAscore(aScore + 7);
+  const aGoal = () => {
+    setAscore(aScore + 10);
   }
 
-  const hField = () => {
-    setHscore(hScore + 3);
+  const hCatchSnitch = () => {
+    setHscore(hScore + 150);
+    alert('Ravenclaw has won the Game!');
   }
 
-  const aField = () => {
-    setAscore(aScore + 3);
+  const aCatchSnitch = () => {
+    setAscore(aScore + 150);
+    alert('Slytherin has won the Game!');
+  }
+
+  const hLookSnitch = () => {
+    let random = Math.floor(Math.random() * (max - min));
+    random === 1 ? hCatchSnitch() : console.log(random + 'Snitch was not found');
+  }
+
+  const aLookSnitch = () => {
+    let random = Math.floor(Math.random() * (max - min));
+    random === 1 ? aCatchSnitch() : console.log(random + 'Snitch was not found');
   }
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Ravenclaw</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
             <div className="home__score">{hScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">00:00:00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">Slytherin</h2>
             <div className="away__score">{aScore}</div>
           </div>
         </div>
@@ -47,12 +61,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={hTouchdown}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={hField}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={hGoal}>Ravenclaw Score Goal</button>
+          <button className="homeButtons__fieldGoal" onClick={hLookSnitch}>Ravenclaw look for Snitch</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={aTouchdown}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={aField}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={aGoal}>Slytherin Score Goal</button>
+          <button className="awayButtons__fieldGoal" onClick={aLookSnitch}>Slytherin look for Snitch</button>
         </div>
       </section>
     </div>
